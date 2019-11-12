083e356577ed9a42cb0902bc55240fb74475e0ea
Noelle Everage <neverage84@yahoo.com>
2019-11-11 20:01:18 -0800

    Added OMDB search to movie-this switch

--- LIRI/liri.js
+++ LIRI/liri.js
@@ -18,9 +18,9 @@ switch (task) {
   case "spotify-this-song":
     runSpotify();
 
+  case "movie-this":
+    runMovie();
 
-  default:
-    console.log("That is not a valid command");
 }
 
 
@@ -76,6 +76,45 @@ function runSpotify(){
     }
    
   console.log(data); 
+  });
+
+}
+
+
+function runMovie(){
+// Then run a request with axios to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + lookUp + "&y=&plot=short&apikey=trilogy";
+
+axios.get(queryUrl).then(
+  function(response) {
+    console.log("Title: " + response.data.Title);
+    console.log("Release Year: " + response.data.Year);
+    console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+    console.log("Rotton Tomatoes Rating: " + response.data.Ratings[1].Value);
+    console.log("Country where Produces: " + response.data.Country);
+    console.log("Movie Language: " + response.data.Language);
+    console.log("Plot: " + response.data.Plot);
+    console.log("Actors: " + response.data.Actors);
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
   });
 
 }
